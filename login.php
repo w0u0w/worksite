3d780a5e4484c1206710e3c55acf524a83f1786e
Vasiliy <w0u0w@users.noreply.github.com>
2021-04-30 03:18:04 +0300

    login form 25%

---
+++ login.php
@@ -0,0 +1,36 @@
+﻿<?php
+
+include "db_connect.php";
+    if(isset($_POST['email']) && isset($_POST['password'])) {
+        function validate($data) {
+            $data = trim($data);
+            $data = stripslashes($data);
+            $data = htmlspecialchars($data);
+            return $data;
+        }
+        $femail = validate($_POST['email']);
+        $fpasswrd = validate($_POST['password']);
+
+        if (empty($femail)) {
+            // header("Location: loginform.php?error=User Name is required");
+            // exit();
+        } 
+        else if(empty($fpasswrd)){
+            // header("Location: loginform.php?error=User Password is required");
+            // exit();
+        }
+        else {
+            $sql = "SELECT * FROM emp WHERE email='$femail' AND password='$fpassword'";
+            $res = mysql_query($sql);
+
+            if(mysql_fetch_array($res)){
+                echo "<h1>dddd</h1>";
+            }
+
+        }
+    }
+    else {
+        header("Location: loginform.php");
+        exit();
+    }
+?>
